6d40711c6ae2fe51135b041c69e545c46589c058
IraLivor <IraLivor@gmail.com>
2013-08-15 17:24:08 -0700

    Added GraveYard File

---
+++ src/decks/subclasses/GraveYard.java
@@ -0,0 +1,17 @@
+package decks.subclasses;
+
+import decks.Deck;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: PerkinsE
+ * Date: 8/15/13
+ * Time: 5:21 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class GraveYard extends Deck {
+
+    GraveYard()
+    {}
+
+}
